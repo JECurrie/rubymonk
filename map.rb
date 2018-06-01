614666f13d2994fcf10592bc496b2b31d7f66785
Joan Currie <jecurrie8@gmail.com>
2018-06-01 02:11:56 +0000

    Ruby test

--- map.rb
+++ map.rb
@@ -1,4 +1,12 @@
 
 #Ruby aliases the method Arr#map to Array#collect
 puts [1, 2, 3, 4, 5].map { |i| i + 1 }
-[1, 2, 3, 4, 5].map { |i| 3 * i }
+puts [1, 2, 3, 4, 5].map { |i| 3 * i }
+
+# select even numbers
+[1,2,3,4,5,6].select {|number| number % 2 == 0}
+
+names = ['rock', 'paper', 'scissors', 'lizard', 'spock']
+names.select {|name| name.length > 5  }
+
+[1,2,3,4,5,6,7,8,9].delete_if{|n| n%2 == 0}
